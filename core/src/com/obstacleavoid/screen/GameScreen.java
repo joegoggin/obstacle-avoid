479560e998ad4279a9c4ab6737ac6bb9e3c3fb63
Joe <joegoggin27@gmail.com>
2019-04-19 16:53:23 -0400

    Lesson 62: Player Class

--- core/src/com/obstacleavoid/screen/GameScreen.java
+++ core/src/com/obstacleavoid/screen/GameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.obstacleavoid.config.GameConfig;
+import com.obstacleavoid.entity.Player;
 import com.obstacleavoid.util.GdxUtils;
 import com.obstacleavoid.util.ViewportUtils;
 
@@ -16,22 +17,45 @@ public class GameScreen implements Screen {
     private Viewport viewport;
     private ShapeRenderer renderer;
 
+    private Player player;
+
     @Override
     public void show() {
         camera = new OrthographicCamera();
         viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera);
         renderer = new ShapeRenderer();
+
+        // create player
+        player = new Player();
+
+        // calculate position
+        float startPlayerX = GameConfig.WORLD_WIDTH / 2;
+        float startPlayerY = 1;
+
+        // position player
+        player.setPosition(startPlayerX, startPlayerY);
     }
 
     @Override
     public void render(float delta) {
         GdxUtils.clearScreen();
 
+        renderDebug();
+    }
+
+    private void renderDebug() {
+        renderer.setProjectionMatrix(camera.combined);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+
         drawDebug();
+
+        renderer.end();
+
+        ViewportUtils.drawGrid(viewport, renderer);
     }
 
     private void drawDebug() {
-        ViewportUtils.drawGrid(viewport, renderer);
+        player.drawDebug(renderer);
     }
 
     @Override
@@ -41,7 +65,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        viewport.update(width, height, true);
         ViewportUtils.debugPixelPerUnit(viewport);
     }
 
